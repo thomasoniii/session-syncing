98faecdce46248fc22cbb7a673a64895daa62c5f
Jim Thomason <jim@japanesewartuba.com>
2020-03-07 14:25:13 -0600

    Updates

---
+++ precache-manifest.fe2a13d9b19b0889f96142945f2fc2cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7dbf3dd6221d754e36d2fa34da3d04a",
+    "url": "/session-syncing/index.html"
+  },
+  {
+    "revision": "23f32a14b0030a0d61dd",
+    "url": "/session-syncing/static/css/main.b8e34d1f.chunk.css"
+  },
+  {
+    "revision": "6001625431768eaf29da",
+    "url": "/session-syncing/static/js/2.46113658.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/session-syncing/static/js/2.46113658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f32a14b0030a0d61dd",
+    "url": "/session-syncing/static/js/main.0c36f4c0.chunk.js"
+  },
+  {
+    "revision": "879f0bfd83a582571dac",
+    "url": "/session-syncing/static/js/runtime-main.1b3b410d.js"
+  }
+]);
